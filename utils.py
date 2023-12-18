37a0e2c56c05f86893759235f82a4b36aee93bf3
jaehyun-jeong <jj.for.jaehyun@gmail.com>
2023-12-18 20:45:27 +0900

    finished Bag of Words

--- utils.py
+++ utils.py
@@ -20,57 +20,71 @@ def one_hot(
     for featureName in nominalFeatures:
 
         # Remove original feature
-        # ex) 
-        # Remove 'season' => Add 'spring', 'summer', 'autumn', 'winter'
+        # ex) # Remove 'season' => Add 'spring', 'summer', 'autumn', 'winter'
         del oneHotData[featureName]
 
         # Loop for unique values in "data".
         for newFeature in data[featureName].unique():
             newFeatureData = {}
-            for dataIdx, label in enumerate(df[featureName]):
+            for dataIdx, label in enumerate(data[featureName]):
                 if label == newFeature:
                     newFeatureData[dataIdx] = 1
                 else:
                     newFeatureData[dataIdx] = 0
 
-            oneHotData[newFeature] = newFeatureData
-
-    print(oneHotData)
+            oneHotData[f"{featureName}_{newFeature}"] = newFeatureData
 
     return pd.DataFrame(data=oneHotData)
 
 
+# Count word from string
+def count_word(string: str, word: str) -> int:
+
+    lenWord = len(word)
+    lenString = len(string)
+    count = 0
+
+    for i in range(lenString - lenWord + 1):
+        if string[i : i + lenWord] == word:
+            count += 1
+
+    return count
+
+
+def bag_of_words(
+    data: pd.DataFrame,
+    textFeature: list,
+    words: list
+):
+
+    # Copy
+    vectorizedData = data.to_dict()
+
+    for featureName in textFeature:
+        
+        # Convert text feature to vectorized feature
+        del vectorizedData[featureName]
+
+        # Loop for each words
+        for word in words:
+            vectorizedFeatureData = {}
+            for dataIdx, string in enumerate(data[featureName]):
+                vectorizedFeatureData[dataIdx] = count_word(string, word)
+
+            vectorizedData[word] = vectorizedFeatureData
+
+    return pd.DataFrame(data=vectorizedData)
+
+
 if __name__ == "__main__":
 
+    '''
     df = pd.read_excel("./Datasets/snow_data.xlsx")
-    df = one_hot(df, ['season'])
+    df = one_hot(df, ['season', 'snow'])
     print(df)
+    '''
 
-'''
-featureName = 'season'
-temp_dict = {}
-temp_list = []
-label_num = len(df[featureName].unique())
-
-for new_col in df[featureName].unique():
-
-    temp_list = []
-
-    for label in df[featureName]:
-        if label == new_col:
-            temp_list.append(1)
-        else:
-            temp_list.append(0)
-
-    temp_dict[new_col] = temp_list
-
-one_hot_df = pd.DataFrame(data=temp_dict)
-
-
-print(df)
-df = df.drop(featureName, axis=1)
-df = pd.concat([df, one_hot_df], axis=1)
-print("=============================================")
-print(df)
-df.to_excel("./snow_data_one_hot.xlsx")
-'''
+    df = pd.read_csv("./Datasets/positive_negative_sentences.tsv", sep='\t', header=0)
+    print(df)
+    df = bag_of_words(df, ["sentence"], ["좋아", "싫어"])
+    print(df)

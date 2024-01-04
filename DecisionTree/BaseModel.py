d414c35b59bf0c217664647c2fa0b91c70d75c95
jaehyun-jeong <jj.for.jaehyun@gmail.com>
2024-01-04 22:26:41 +0900

    DecisionTree: Fixed comments of BaseModel.py

--- DecisionTree/BaseModel.py
+++ DecisionTree/BaseModel.py
@@ -2,54 +2,40 @@ from typing import Type, Tuple
 import numpy as np
 
 
+# 트리의 노드를 표현하는 클래스
 class Node:
 
     def __init__(
         self,
-        feature: int = None,  # 피처의 인덱스, Branch 노드면 필요
-        threshold: float = None,  # 임계값, Branch 노드면 필요
-        classIdx: int = None  # 분류할 클래스, Leaf 노드면 필요
+        feature: int = None,  # 피처의 인덱스, Branch 노드면 필요하다.
+        threshold: float = None,  # 임계값, Branch 노드면 필요하다.
+        classIdx: int = None  # 분류할 클래스, Leaf 노드면 필요하다.
     ):
 
-        self._left: Type[Node] = None  # 왼쪽 노드
-        self._right: Type[Node] = None  # 오른쪽 노드
+        self.left: Type[Node]  # 왼쪽 노드
+        self.right: Type[Node]  # 오른쪽 노드
 
-        # Branch 노드를 위한 인스턴스
+        # Branch 노드를 위한 인스턴스 변수
         self.feature: int = feature
         self.threshold: float = threshold
 
-        # Leaf 노드를 위한 인스턴스
+        # Leaf 노드를 위한 인스턴스 변수
         self.classIdx: int = classIdx
 
-    @property
-    def left(self):
-        return self._left
-
-    @left.setter
-    def left(self, node):
-        self._left = node
-
-    @property
-    def right(self):
-        return self._right
-
-    @right.setter
-    def right(self, node):
-        self._right = node
-
-    # Leaf 노드인지, Branch 노드인지 확인
+    # Leaf 노드인지, Branch 노드인지 확인한다.
     def is_leaf(self) -> bool:
         return False if self.classIdx is None else True
 
 
+# 모든 Tree의 부모 클래스
 class TreeBased():
 
     def __init__(
         self
     ):
 
-        self._numFeatures: int = None
-        self._root: Type(Node) = None
+        self._numFeatures: int = None  # 피쳐의 수
+        self._root: Type(Node) = None  # 루트 노드
 
     # 각 클래스당 데이터 수를 dictionary 형태로 리턴한다.
     # ex)
@@ -63,10 +49,14 @@ class TreeBased():
 
         return dict(zip(uniqueClasses, countsClasses))
 
+    # 'Y'에서 가장 많은 클래스의 인덱스를 리턴한다.
     @staticmethod
     def __select_class(
         Y: np.ndarray
-    ):
+    ) -> np.int_:
+
+        # 'Y'에서 가장 많은 인덱스를 "selectClass"에 저장한다.
+        selectClass: np.int
 
         try:
             selectClass = np.bincount(Y).argmax()
@@ -75,28 +65,35 @@ class TreeBased():
 
         return selectClass
 
+    # 인덱스가 'featureIdx'인 feature의 'threshold'를 기준으로 데이터를 나눈다.
     @staticmethod
     def __split(
         X: np.ndarray,
         featureIdx: int,
         threshold: float
-    ) -> Tuple[int, int]:
+    ) -> Tuple[list[int], list[int]]:
 
+        # feature를 고른다.
         XFeature = X[:, featureIdx]
+
+        # "threshold"를 기준으로한 X의 인덱스 리스트를 만든다.
         leftIdxs = np.argwhere(XFeature < threshold).flatten()
         rightIdxs = np.argwhere(XFeature >= threshold).flatten()
 
         return leftIdxs, rightIdxs
 
+    # 지니계수를 계산한다.
     @staticmethod
     def gini_impurity(
         Y: np.ndarray
     ) -> float:
 
-        giniImpurity: float = 1
-        listClasses: np.ndarray = np.unique(Y)
-        sizeY: int = len(Y)
+        # 지니계수는 1에서 빼는 순서로 계산되므로 1로 초기화한다.
+        giniImpurity: float = 1  # 지니계수
+        listClasses: np.ndarray = np.unique(Y)  # 클래스 종류의 배열
+        sizeY: int = len(Y)  # 'Y'의 크기
 
+        # 지니계수를 계산한다.
         for classIdx in listClasses:
             giniImpurity -= (np.sum(Y == classIdx) / sizeY)**2
 

cfaf32f7dbd5343a984de0b45fc7ef7a76ad77b9
jaehyun-jeong <jj.for.jaehyun@gmail.com>
2024-01-30 17:28:30 +0900

    KNN: finished

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -58,30 +58,60 @@ class KNN:
             raise ValueError("\"X\" must be the 1d array!")
 
         # 거리를 구한다.
-        norm = self._norm(x)
+        norm = list(self._norm(x))
 
-        sortedNormIdxs = self.sort_idxs(list(norm), sortedNormIdxs)
+        # 거리의 인덱스를 거리 순으로 정렬한다.
+        # ex)
+        # 거리: [2.0, 4.0, 4.0, 4.0, 4.0, 3.0, 4.0, 4.0, 6.0, 3.0, 5.0, 5.0]
+        # 인덱스: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        # 실행 후
+        # 거리: [2.0, 4.0, 4.0, 4.0, 4.0, 3.0, 4.0, 4.0, 6.0, 3.0, 5.0, 5.0]
+        # 인덱스: [0, 5, 9, 1, 2, 3, 4, 6, 7, 10, 11, 8]
+        sortedNormIdxs = self.sort_idxs(norm, sortedNormIdxs)
+
+        # 인덱스를 참조하여 가장 많은 수의 클래스를 출력한다.
+        return np.bincount(self.Y[sortedNormIdxs[:self.k]]).argmax()
 
     # Quick sort 를 사용해서 정렬한다.
-    @staticmethod
     def sort_idxs(
+        self,
         # X 를 소트하고, 바뀐 순서를 idxs에 저장한다.
         values: list,
-        idxs: list
-    ):
+        idxs: list,
+    ) -> list:
 
         pivot: float  # Quick Sort 의 pivot
 
-        if(1 < len(values)):
+        leftValues: list = []
+        rightValues: list = []
+        leftIdxs: list = []
+        rightIdxs: list = []
+
+        if len(values) <= 1:
+            return idxs
+        else:
+
+            # pivot 으로 0 번째 숫자를 선택한다.
             pivot = values[0]
-            pass
+
+            for idx, value in zip(idxs[1:], values[1:]):
+                if value < pivot:
+                    leftValues.append(value)
+                    leftIdxs.append(idx)
+                else:
+                    rightValues.append(value)
+                    rightIdxs.append(idx)
+
+            return self.sort_idxs(leftValues, leftIdxs) + [idxs[0]] + \
+                self.sort_idxs(rightValues, rightIdxs)
+
 
 if __name__ == "__main__":
     from pandas import read_csv
 
-    df = read_csv("../Datasets/zebra_giraffe_crowd_index.csv")
+    df = read_csv("../Datasets/zebra_giraffe_crowd_noise_index.csv")
     X = df[['x', 'y']].to_numpy()
     Y = df['species'].to_numpy()
 
-    model = KNN(X, Y, k=3, p=1)
-    model.predict(list(X[0]))
+    model = KNN(X, Y, k=1, p=1)
+    print(model.predict([0, 0]))
